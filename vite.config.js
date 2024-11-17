41ac9ca3a57b8bc001cacda7162cb51e257d33b1
gwagjiug <kwjo0228@naver.com>
2024-11-17 13:09:22 +0900

    init : eslint 설정

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import eslintPlugin from "vite-plugin-eslint"; // eslintPlugin import 추가
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()], // eslint 플러그인 추가
+  plugins: [react(), eslintPlugin()],
 });
