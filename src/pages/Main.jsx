d7be7d33d77d0a46cf8ad4a36c96a9b748fd653f
gwagjiug <kwjo0228@naver.com>
2024-11-20 13:40:07 +0900

    refactor : Main 페이지 코드 구조 개선

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,26 +5,22 @@ import Poster from '../components/features/main/poster/Poster';
 import HotFeed from '../components/Features/comments/HotFeed';
 
 function Main() {
-  const movieTitle = [
-    '개인 추천 영화',
-    '필밋 TOP 10',
-    '공개 예정작',
-    '박스오피스 순위',
-    '지금 뜨는 코멘트',
+  const movieSections = [
+    { title: '개인 추천 영화', component: <Poster /> },
+    { title: '필밋 TOP 10', component: <Poster /> },
+    { title: '공개 예정작', component: <Poster /> },
+    { title: '박스오피스 순위', component: <Poster /> },
+    { title: '지금 뜨는 코멘트', component: <HotFeed /> },
   ];
 
   return (
     <MainBody>
-      <Title>{movieTitle[0]}</Title>
-      <Poster />
-      <Title>{movieTitle[1]}</Title>
-      <Poster />
-      <Title>{movieTitle[2]}</Title>
-      <Poster />
-      <Title>{movieTitle[3]}</Title>
-      <Poster />
-      <Title>{movieTitle[4]}</Title>
-      <HotFeed />
+      {movieSections.map(({ title, component }, index) => (
+        <React.Fragment key={index}>
+          <Title>{title}</Title>
+          {component}
+        </React.Fragment>
+      ))}
     </MainBody>
   );
 }
