dbb0d2202c38114ea90b4aa497ba3a3c00b61f63
gwagjiug <kwjo0228@naver.com>
2024-11-26 18:46:51 +0900

    init: 쿼리 의존성 추가 및 devtools 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import router from './routes/router';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import UserModal from './components/Common/modal/userModal';
+import { QueryClient, QueryClientProvider, ReactQueryDevtools } from '@tanstack/react-query';
+
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <UserModal />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <UserModal />
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </>
   );
 }
