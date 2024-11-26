ec44459479156c71f67717dc7513fa20a1e4ed18
gwagjiug <kwjo0228@naver.com>
2024-11-26 11:30:13 +0900

    fix: build 에러 해결

--- src/components/Common/header/Header.jsx
+++ src/components/Common/header/Header.jsx
@@ -4,22 +4,6 @@ import Button from './Button';
 import useModalStore from '../../../store/modal/useModalStore';
 import { useState } from 'react';
 
-const DefaultHeader = styled.header`
-  display: flex;
-  justify-content: space-between;
-  background-color: ${lightTheme.mainColor};
-  border-bottom: ${lightTheme.borderDefault};
-  height: 5rem;
-  padding: 1rem 20rem;
-  box-sizing: border-box;
-`;
-
-const HeaderSection = styled.div`
-  width: 12rem;
-  display: flex;
-  align-items: center;
-`;
-
 function Header() {
   const { openModal } = useModalStore();
   const [activeButton, setActiveButton] = useState(null);
@@ -43,11 +27,29 @@ function Header() {
     ));
 
   return (
-    <DefaultHeader>
-      <HeaderSection>{renderButtons(buttons, true)}</HeaderSection>
-      <HeaderSection>{renderButtons(authButtons)}</HeaderSection>
-    </DefaultHeader>
+    <S.DefaultHeader>
+      <S.HeaderSection>{renderButtons(buttons, true)}</S.HeaderSection>
+      <S.HeaderSection>{renderButtons(authButtons)}</S.HeaderSection>
+    </S.DefaultHeader>
   );
 }
+
+const S = {
+  DefaultHeader: styled.header`
+    display: flex;
+    justify-content: space-between;
+    background-color: ${lightTheme.mainColor};
+    border-bottom: ${lightTheme.borderDefault};
+    height: 5rem;
+    padding: 1rem 20rem;
+    box-sizing: border-box;
+  `,
+
+  HeaderSection: styled.div`
+    width: 12rem;
+    display: flex;
+    align-items: center;
+  `,
+};
 
 export default Header;
