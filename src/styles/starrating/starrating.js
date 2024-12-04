303a11330fa55f5582404bca5bd9541b53ecd19c
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2024-12-05 00:49:08 +0900

    style : 평가하기 페이지 퍼블리싱

---
+++ src/styles/starrating/starrating.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StarRatingPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${(props) => props.theme.color.mainColor};
+`;
