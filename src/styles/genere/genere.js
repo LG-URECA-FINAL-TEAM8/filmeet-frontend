f76eedc56e2642715879b76af5102acd542f083b
LeeSunghuck <hello.git.anaolfive@gmail.com>
2024-12-04 15:06:27 +0900

    fix : px 를 rem으로 변경완료

--- src/styles/genere/genere.js
+++ src/styles/genere/genere.js
@@ -1,18 +1,19 @@
 import styled from "styled-components";
 
 export const GenrePageWrapper = styled.div`
-   display: flex;
-  flex-direction: column; /* 세로로 정렬 */
-  align-items: center; /* 가운데 정렬 */
-  gap: 20px; /* 섹션 간 간격 */
-  padding: 20px; /* 페이지 안쪽 여백 */
-  background-color: #F0F0F3;
-  min-height: 100vh; /* 화면 전체 높이 최소 확보 */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.25rem;
+  padding: 1.25rem;
+  background-color: #f0f0f3;
+  min-height: 100vh;
 `;
+
 export const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 20px;
-  gap: 20px;
+  gap: 1.25rem;
+  padding: 1.25rem;
 `;
