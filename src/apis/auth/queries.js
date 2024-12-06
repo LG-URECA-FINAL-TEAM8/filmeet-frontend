ee6c8f6c9d5e0df9e5b8df18c07d4692216a1dd9
gwagjiug <kwjo0228@naver.com>
2024-12-06 14:38:33 +0900

    fix : 로그인 회원가입 query key 변경

--- src/apis/auth/queries.js
+++ src/apis/auth/queries.js
@@ -7,7 +7,7 @@ export const useSignUp = () => {
   return useMutation({
     mutationFn: SignUp,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['user'] });
+      queryClient.invalidateQueries({ queryKey: ['userSignUp'] });
     },
     onError: (error) => {
       return error;
@@ -20,7 +20,7 @@ export const useLogin = () => {
   return useMutation({
     mutationFn: Login,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['user'] });
+      queryClient.invalidateQueries({ queryKey: ['userLogin'] });
     },
     onError: (error) => error,
   });
