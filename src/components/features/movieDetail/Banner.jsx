8cfeebd0c56ed599951afb7f29d982a3d1ed0d11
KI HYUN YOON <116802387+spin54@users.noreply.github.com>
2024-12-07 16:19:34 +0900

    fix : 수정중

--- src/components/features/movieDetail/Banner.jsx
+++ src/components/features/movieDetail/Banner.jsx
@@ -1,17 +1,23 @@
 import styled from "styled-components";
 
 function Banner() {
-  const bgImageUrl = 'https://via.placeholder.com/1920x400';
+  const bannerData = {
+    title: "트렁크",
+    subtitle: "한국 시리즈 인기 순위 1위",
+    details: "2024.로맨스/드라마/TV드라마",
+    age: "한국.청불",
+    backgroundUrl: "https://via.placeholder.com/1920x400",
+  };
 
   return (
-    <S.BannerContainer bgImage={bgImageUrl}>
+    <S.BannerContainer bgImage={bannerData.backgroundUrl}>
       <S.BannerContentContainer>
         <S.BannerContent>
-          <S.BannerTitle>트렁크</S.BannerTitle>
-          <S.BannerSubTitle>트렁크</S.BannerSubTitle>
-          <S.BannerSubContent>2024.로맨스/드라마/TV드라마</S.BannerSubContent>
-          <S.BannerSubContent>한국.청불</S.BannerSubContent>
-          <S.BannerSubTitle>한국 시리즈 인기 순위 1위</S.BannerSubTitle>
+          <S.BannerTitle>{bannerData.title}</S.BannerTitle>
+          <S.BannerSubTitle>{bannerData.title}</S.BannerSubTitle>
+          <S.BannerSubContent>{bannerData.details}</S.BannerSubContent>
+          <S.BannerSubContent>{bannerData.age}</S.BannerSubContent>
+          <S.BannerSubTitle>{bannerData.subtitle}</S.BannerSubTitle>
         </S.BannerContent>
       </S.BannerContentContainer>
     </S.BannerContainer>
@@ -22,51 +28,51 @@ export default Banner;
 
 const S = {
   BannerContainer: styled.div`
+    position: relative;
     width: 100%;
     height: 25rem;
-    position: relative;
-    background-color: #000;
     background-image: ${(props) => `url(${props.bgImage})`};
   `,
 
   BannerContentContainer: styled.div`
     position: absolute;
-    width: 1320px;
-    height: 172px;
-    margin: 0 300px;
     top: 45%;
     left: 0;
     display: flex;
     align-items: center;
+    width: 82.5rem;
+    height: 10.75rem;
+    margin: 0 18.75rem;
   `,
 
   BannerContent: styled.div`
-    width: 230px;
-    height: 172px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     text-align: left;
+    width: 14.37rem;
+    height: 10.75rem;
   `,
 
   BannerTitle: styled.h1`
+    margin: 0;
     font-family: ${(props) => props.theme.font.fontSuitBold};
     font-size: 2rem;
     color: ${(props) => props.theme.color.fontWhite};
-    margin: 0 0 0 0;
   `,
 
   BannerSubTitle: styled.div`
+    margin: 1rem 0 0;
     font-family: ${(props) => props.theme.font.fontSuitRegular};
     font-size: 0.8rem;
     color: ${(props) => props.theme.color.fontWhite};
-    margin: 1rem 0 0;
   `,
 
   BannerSubContent: styled.div`
+    margin: 0.5rem 0 0;
     font-family: ${(props) => props.theme.font.fontSuitRegular};
     font-size: 0.8rem;
     color: ${(props) => props.theme.color.fontWhite};
-    margin: 0.5rem 0 0;
   `,
 };
+
--- src/pages/movieDetail/MovieDetail.jsx
+++ src/pages/movieDetail/MovieDetail.jsx
@@ -1,19 +1,9 @@
-import styled from "styled-components";
 import Banner from "../../components/features/movieDetail/Banner";
 import CastAndCrew from "../../components/features/movieDetail/CastAndCrew";
 import Content from "../../components/features/movieDetail/Content";
 import MovieComment from "../../components/features/movieDetail/MovieComment";
-
-const comments = Array.from({ length: 12 }, (_, index) => ({
-  id: index + 1,
-  userImage: "https://via.placeholder.com/40",
-  userName: `User ${index + 1}`,
-  title: `Movie Title ${index + 1}`,
-  comment: `This is a comment for Movie Title ${index + 1}`,
-  rating: Math.random() * 5,
-  likes: Math.floor(Math.random() * 100),
-  comments: Math.floor(Math.random() * 50),
-}));
+import { comments } from "../../data/comments";
+import { PageWrapper } from "../../styles/moviedetail/moviedetail";
 
 function MovieDetail() {
   return (
@@ -22,16 +12,11 @@ function MovieDetail() {
         <Banner />
         <Content />
       </PageWrapper>
-      <CastAndCrew />
-      <MovieComment comments={comments} />
+        <CastAndCrew />
+        <MovieComment comments={comments} />
     </>
   );
 }
 
 export default MovieDetail;
 
-const PageWrapper = styled.div`
-  background-color: ${(props) => props.theme.color.commentColor};
-  width: 100%;
-  height: 100%;
-`;
