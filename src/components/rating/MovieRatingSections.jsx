db787f55b60d4656edf7b5e40bab6d7292664794
BAMLATTE <jun109209@gmail.com>
2024-12-11 21:48:53 +0900

    Update MovieRatingSections.jsx

--- src/components/rating/MovieRatingSections.jsx
+++ src/components/rating/MovieRatingSections.jsx
@@ -1,5 +1,5 @@
 import { movies } from '../../data/movies';
-import Poster from '../Common/poster/Poster';
+import Poster from '../common/poster/Poster';
 import { groupMoviesByRating } from '../../utils/ratings/groupMoviesRatings';
 import * as S from '../../styles/rating/rating';
 import { useNavigate } from 'react-router-dom';
