c035c6c64d6d515bfd5edffb12a4399a767ce88a
gwagjiug <kwjo0228@naver.com>
2024-12-11 15:12:30 +0900

    feat : 소셜로그인 쿼리 파라미터 추출 함수 구현

--- src/pages/StarRatingPage.jsx
+++ src/pages/StarRatingPage.jsx
@@ -1,14 +1,14 @@
-import StarRatingBody from "../components/StarRating/StarRatingBody";
-import StarRatingHeader from "../components/StarRating/StarRatingHeader";
-import { StarRatingPageWrapper } from "../styles/starrating/starrating";
+import StarRatingBody from '../components/StarRating/StarRatingBody';
+import StarRatingHeader from '../components/StarRating/StarRatingHeader';
+import { StarRatingPageWrapper } from '../styles/starrating/starrating';
 
 const StarRatingPage = () => {
-    return (
-      <StarRatingPageWrapper>
-        <StarRatingHeader/>
-        <StarRatingBody/>
-      </StarRatingPageWrapper>
-    );
-  };
-  
+  return (
+    <StarRatingPageWrapper>
+      <StarRatingHeader />
+      <StarRatingBody />
+    </StarRatingPageWrapper>
+  );
+};
+//영화 평가 페이지(헤더)
 export default StarRatingPage;
