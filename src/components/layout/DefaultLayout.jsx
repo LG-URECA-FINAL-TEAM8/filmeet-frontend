e35fd30b4a5e3da972644c112e0f440fa1d32473
BAMLATTE <jun109209@gmail.com>
2024-12-11 21:28:43 +0900

    Update DefaultLayout.jsx

--- src/components/layout/DefaultLayout.jsx
+++ src/components/layout/DefaultLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import React from 'react';
 
-const Header = React.lazy(() => import('../Common/header/Header'));
+const Header = React.lazy(() => import('../common/header/Header'));
 
 const DefaultLayout = () => {
   return (
