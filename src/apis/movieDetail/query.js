40c022a0f6e267778694b5871439bc9efe188e83
gwagjiug <kwjo0228@naver.com>
2024-12-13 02:36:15 +0900

    feat : 영화 상세 페이지 커멘트 데이터 패칭

--- src/apis/movieDetail/query.js
+++ src/apis/movieDetail/query.js
@@ -1,9 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
-import { getMovieDetail } from './getMovieDetail';
+import { getMovieDetail, getMovieComment } from './getMovieDetail';
 export const useMovieDetail = (movieId) => {
   const { data, isLoading, error } = useQuery({
     queryKey: ['movieDetail', movieId],
     queryFn: () => getMovieDetail(movieId),
+    refetchOnWindowFocus: false,
+  });
+
+  return { data, isLoading, error };
+};
+
+export const useMovieComment = (movieId) => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['movieComment', movieId],
+    queryFn: () => getMovieComment(movieId),
     refetchOnWindowFocus: false,
   });
 
--- src/components/features/movieDetail/MovieComment.jsx
+++ src/components/features/movieDetail/MovieComment.jsx
@@ -1,33 +1,30 @@
-import styled from "styled-components";
-import MiniComment from "./MiniComment";
-import { useNavigate } from "react-router-dom";
-import { movieDetailData } from "../../../data/moviedetail";
+import styled from 'styled-components';
+import MiniComment from './MiniComment';
+import { useNavigate } from 'react-router-dom';
 
-function MovieComment() {
-    const navigate = useNavigate();
+function MovieComment({ comment }) {
+  const navigate = useNavigate();
 
-    const comments = movieDetailData.movieReviewsResponses.content;
+  const handleMoreClick = () => {
+    navigate(`/moviedetail/moviecomment`);
+  };
 
-    const handleMoreClick = () => {
-        navigate(`/moviedetail/moviecomment`);
-      };
-
-    const moviecomment = {
-      comment: "코멘트",
-      more: "더보기",
-    }
+  const moviecomment = {
+    comment: '코멘트',
+    more: '더보기',
+  };
 
   return (
     <S.MovieCommentContainer>
       <S.CommentHeader>
         <S.SectionTitle>
-          {moviecomment.comment} 
-          <S.CommentCount>{comments.length}+</S.CommentCount>
+          {moviecomment.comment}
+          <S.CommentCount>{comment.length}+</S.CommentCount>
         </S.SectionTitle>
         <S.ShowMoreButton onClick={handleMoreClick}>{moviecomment.more}</S.ShowMoreButton>
       </S.CommentHeader>
       <S.CommentGrid>
-        {comments.map((item) => (
+        {comment.map((item) => (
           <MiniComment key={item.reviewId} {...item} />
         ))}
       </S.CommentGrid>
@@ -91,5 +88,3 @@ const S = {
     cursor: pointer;
   `,
 };
-
-
