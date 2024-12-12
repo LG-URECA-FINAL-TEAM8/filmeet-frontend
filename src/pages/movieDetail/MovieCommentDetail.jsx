9f2017c2afd07905ad88ca4949f05be15030125e
gwagjiug <kwjo0228@naver.com>
2024-12-13 02:58:51 +0900

    feat : commentAll 페이지 데이터 패칭 구현

--- src/pages/movieDetail/MovieCommentDetail.jsx
+++ src/pages/movieDetail/MovieCommentDetail.jsx
@@ -2,15 +2,19 @@ import TopHeader from '../../components/Common/back/TopHeader';
 import MovieMoreComment from '../../components/features/movieDetail/MovieMoreComment';
 import { movieDetailData } from '../../data/moviedetail';
 import { CardContainer, CommentPageContainer } from '../../styles/comment/comment';
-
+import { useParams } from 'react-router-dom';
+import { useMovieComment } from '../../apis/movieDetail/query';
 const MovieCommentDetail = () => {
   const comments = movieDetailData.movieReviewsResponses.content;
+  const { id } = useParams();
+  const { data: movieComment } = useMovieComment(id);
+  const movieCommentData = movieComment?.data?.content || [];
 
   return (
     <CommentPageContainer>
       <TopHeader title="코멘트" />
       <CardContainer>
-        <MovieMoreComment comments={comments} />
+        <MovieMoreComment comments={movieCommentData} />
       </CardContainer>
     </CommentPageContainer>
   );
