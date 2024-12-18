b7deef05cf9c00b1eeed5b40f86dc8a8a08e7807
gwagjiug <kwjo0228@naver.com>
2024-12-18 18:16:59 +0900

    delete : 파일삭제

--- src/store/admin/usePageStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-const usePagestore = create((set) => ({
-  currentPage: 'movieManagement',
-  setPage: (page) => set({ currentPage: page }),
-}));
-
-export default usePagestore;
