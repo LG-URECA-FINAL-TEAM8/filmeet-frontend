6fa4a9d4d6423a5f2c80220f422820010c699c2a
gwagjiug <kwjo0228@naver.com>
2024-12-18 14:52:23 +0900

    feat : 팔로워 페이지 팔로우기능 구현

--- src/components/follow/FollowerList.jsx
+++ src/components/follow/FollowerList.jsx
@@ -1,14 +1,19 @@
 import * as S from '../../styles/follow/follow';
 import { useFollowers } from '../../apis/myPage/follow/queries';
+import { useAddFollow } from '../../apis/follow/query';
 
 const FollowersList = ({ userId }) => {
   const { data, isLoading, error } = useFollowers(userId);
+  const { mutate: addFollow } = useAddFollow();
 
   if (isLoading) return <div>로딩 중...</div>;
   if (error) return <div>오류가 발생했습니다: {error.message}</div>;
 
   const followers = data?.data?.content || [];
 
+  const handleFollowClick = (followerId) => {
+    addFollow({ userId: followerId });
+  };
   return (
     <S.ListWrapper>
       {followers.map((follower) => (
@@ -21,7 +26,7 @@ const FollowersList = ({ userId }) => {
           </S.AvatarWrapper>
           <S.InfoWrapper>
             <S.Name>{follower.nickname}</S.Name>
-            <S.FollowButton>팔로우</S.FollowButton>
+            <S.FollowButton onClick={() => handleFollowClick(follower?.id)}>팔로우</S.FollowButton>
           </S.InfoWrapper>
         </S.ListItem>
       ))}
