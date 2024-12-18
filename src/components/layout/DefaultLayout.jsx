d04d0aec451fcc0f674eb17cd59199d06b8493dc
gwagjiug <kwjo0228@naver.com>
2024-12-18 23:57:37 +0900

    feat : useScrollToTop 구현

--- src/components/layout/DefaultLayout.jsx
+++ src/components/layout/DefaultLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import React from 'react';
-
+import useScrollToTop from '../../hooks/useScrollToTop';
 const Header = React.lazy(() => import('../common/header/Header'));
 
 const DefaultLayout = () => {
+  useScrollToTop();
   return (
     <>
       <Header />
